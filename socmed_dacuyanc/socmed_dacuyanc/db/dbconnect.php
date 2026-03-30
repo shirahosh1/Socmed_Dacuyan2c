b3de5d277355a1d812a02b6d863e8c3869b5771d
Syrell Jane S. Dacuyan <xyriljn@gmail.com>
2026-03-30 23:20:25 +0800

    Delete socmed_dacuyanc directory

--- socmed_dacuyanc/socmed_dacuyanc/db/dbconnect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$host="localhost";
-$username="root";
-$dbpassword="";
-$dbname="socmed_dacuyanc";
-
-$con = new mysqli($host,$username,$dbpassword,$dbname);
--- socmed_dacuyanc/socmed_dacuyanc/timeline.php
+++
@@ -1,7 +0,0 @@
-<?php include 'views/header.php'; ?>
-
-<div class="container">
-    <?php include 'models/load_timeline.php'; ?>
-</div>
-
-<?php include 'views/footer.php'; ?>;
